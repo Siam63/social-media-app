c984d7bd7d5ee09c8c8609387560101694f2e32a
siamr <siam_1000@hotmail.com>
2024-01-05 17:56:50 -0500

    offline and online status change

--- src/Main/Card.jsx
+++ src/Main/Card.jsx
@@ -5,9 +5,11 @@ const Card = ({ name, img, status }) => {
         <div>
             <div className="relative">
                 <img className="h-80 w-56 rounded-2xl hover:scale-105 duration-700 ease-in-out cursor-pointer shadow-lg" src={img} alt={name}></img>
+                
                 <p className="font-roboto bottom-4 left-4 text-sm font-medium text-white absolute no-underline leading-none">{name}</p>
-                <p className={`${status === "Offline" ? "absolute bottom-4 right-4 text-sm font-medium text-red-600 font-roboto no-underline leading-none" :
-                 "absolute bottom-4 right-4 text-sm font-medium text-green-600 font-roboto no-underline leading-none"}`}>{status}</p>
+                
+                <p className={`${status === "Offline" ? "absolute mr-2 bottom-4 right-4 w-3 h-3 bg-red-600 rounded-full" :
+                    "absolute mr-2 bottom-4 right-4 w-3 h-3 bg-green-500 rounded-full"}`}></p>
             </div>
         </div>
     )
